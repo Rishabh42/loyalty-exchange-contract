d66f8f31509624d5e1b131baba3e6249797cd65e
Rishabh42 <rishabhthaney@gmail.com>
2018-07-03 16:07:56 +0530

    added details and functions for users & companies

--- contracts/loyalty.sol
+++ contracts/loyalty.sol
@@ -3,14 +3,49 @@ pragma solidity ^0.4.17;
 contract loyalty {
   address public owner;
 
-  mapping (uint => address) public companies;
+struct User {
+    uint256 Uid;
+    bytes32 Uname;
+    bool Uset;
+}
+
+struct Company {
+  uint256 Cid;
+  bytes32 Cname;
+  bool Cset;
+}
+
+  mapping(address => User) public users;
+  mapping(address => Company) public companies;
 
   constructor loyalty() public{
     owner = msg.sender;
   }
 
-  function newEntry(uint cName) public {
-    companies[cName] = msg.sender;
+  function createUser(uint256 _userId, bytes32 _userName) public {
+      address _userAddress = msg.sender;
+      User storage user = users[_userAddress];
+      // Check that the user did not already exist:
+      require(!user.Uset);
+      //Store the user
+      users[_userAddress] = User({
+          Uid: _userId,
+          Uname: _userName,
+          Uset: true
+      });
   }
 
+  function createCompany(uint256 _companyId, bytes32 _companyName) public {
+      address _companyAddress = msg.sender;
+
+      Company storage company = companies[_companyAddress];
+
+      require(!company.Cset);
+
+      companies[_companyAddress] = Company({
+          Cid: _companyId,
+          Cname: _companyName,
+          Cset: true
+      });
+  }
 }
