9a4b0ba2bc111723dae8cb40504bdda842d7cbe3
Rishabh42 <rishabhthaney@gmail.com>
2018-07-03 10:57:28 +0530

    removed struct

--- contracts/loyalty.sol
+++ contracts/loyalty.sol
@@ -2,11 +2,7 @@ pragma solidity ^0.4.17;
 
 contract loyalty {
   address public owner;
-  struct{
-    string name;
-    string email;
-    
-  }
+
   mapping (uint => address) public companies;
 
   constructor loyalty() public{
