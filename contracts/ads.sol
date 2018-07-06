5daff2c0236ba309e161198af7a77dfd8b8d5990
Rishabh42 <rishabhthaney@gmail.com>
2018-07-06 17:24:53 +0530

    new ad constructor, changed transfer fx

--- contracts/ads.sol
+++ contracts/ads.sol
@@ -1,9 +1,25 @@
 pragma solidity ^0.4.17;
 
 contract ads {
+  address[] public redeemed;
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-  function transferFrom(address _from, address _to, uint256 _amount){
-    returns (bool success)
+  constructor ads(string _ad, uint price) public payable{
+    require(msg.value > price);
+    redeemed.push(msg.sender);
   }
+  
+  function transfer(address _to, uint256 _amount) returns (bool success) {
+        if (balances[msg.sender] >= _amount
+            && _amount > 0
+            && balances[_to] + _amount > balances[_to]) {
+            balances[msg.sender] -= _amount;
+            balances[_to] += _amount;
+            Transfer(msg.sender, _to, _amount);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
