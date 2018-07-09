3bf677759f612144c1f6caedae5baac5c38f5e62
Rishabh42 <rishabhthaney@gmail.com>
2018-07-09 15:20:17 +0530

    uncommented display user

--- contracts/loyalty.sol
+++ contracts/loyalty.sol
@@ -49,11 +49,11 @@ struct Company {
       });
   }
 
-  function balanceOf(address _owner) constant returns (uint256 balance) {
+    function balanceOf(address _owner) constant returns (uint256 balance) {
         return balances[_owner];
     }
 
-/*  function getUsers() public view returns (string) {
-        return users[msg.sender].name;
-    } */
+    function getUsers() public view returns (string) {
+        return users[msg.sender];
+    }
 }
